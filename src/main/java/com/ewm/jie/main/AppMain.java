b8f02623ba0d25d302391fa29682b2a97cb2cfde
jackie.chen <jackie.chen@qunar.com>
2016-09-05 16:24:34 +0800

    init

--- src/main/java/com/ewm/jie/main/AppMain.java
+++ src/main/java/com/ewm/jie/main/AppMain.java
@@ -1,7 +1,20 @@
 package com.ewm.jie.main;
 
+import com.alibaba.fastjson.JSONObject;
+import com.ewm.jie.util.MatrixHelper;
+
 /**
  * Created by jackie.chen on 2016/8/26.
  */
 public class AppMain {
+
+    public static void main(String[] args) {
+        // 生成二维码
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("auther","jackie.chen");
+        jsonObject.put("url","http://www.baidu.com");
+        MatrixHelper.str2Ewm(jsonObject.toJSONString());
+        // 解析二维码
+        MatrixHelper.ewm2Str(null);
+    }
 }
--- src/main/java/com/ewm/jie/util/MatrixHelper.java
+++ src/main/java/com/ewm/jie/util/MatrixHelper.java
@@ -1,28 +1,61 @@
 package com.ewm.jie.util;
 
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Hashtable;
+import java.util.Map;
 
-import com.ewm.jie.util.MatrixToImageWriter;
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.EncodeHintType;
-import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.*;
+import com.google.zxing.client.j2se.BufferedImageLuminanceSource;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
+import com.google.zxing.common.HybridBinarizer;
+
+import javax.imageio.ImageIO;
 
 public class MatrixHelper {
 
-    public static void main(String[] args) throws Exception {
-        String text = "I Love You！"; // 二维码内容
+    public static void str2Ewm(String text) {
+//        String text = ""; // 二维码内容
         int width = 300; // 二维码图片宽度  
         int height = 300; // 二维码图片高度  
         String format = "gif";// 二维码的图片格式  
 
         Hashtable<EncodeHintType, String> hints = new Hashtable<EncodeHintType, String>();
         hints.put(EncodeHintType.CHARACTER_SET, "utf-8");   // 内容所使用字符集编码  
+        try {
+            BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height, hints);
+            // 生成二维码
+            File outputFile = new File("d:" + File.separator + "new.jpg");
+            MatrixToImageWriter.writeToFile(bitMatrix, format, outputFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-        BitMatrix bitMatrix = new MultiFormatWriter().encode(text, BarcodeFormat.QR_CODE, width, height, hints);
-        // 生成二维码  
-        File outputFile = new File("d:" + File.separator + "new.gif");
-        MatrixToImageWriter.writeToFile(bitMatrix, format, outputFile);
+    public static void ewm2Str(String filePath) {
+        filePath = "D://new.jpg";
+        BufferedImage image;
+        try {
+            image = ImageIO.read(new File(filePath));
+            LuminanceSource source = new BufferedImageLuminanceSource(image);
+            Binarizer binarizer = new HybridBinarizer(source);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(binarizer);
+            Map<DecodeHintType, Object> hints = new HashMap<DecodeHintType, Object>();
+            hints.put(DecodeHintType.CHARACTER_SET, "UTF-8");
+            Result result = new MultiFormatReader().decode(binaryBitmap, hints);// 对图像进行解码
+            System.out.println("图片中内容：  " + result.getText());
+            System.out.println("encode： " + result.getBarcodeFormat());
+            System.out.println("times： " + result.getTimestamp());
+            for (ResultPoint resultPoint : result.getResultPoints()) {
+                System.out.println("point： " + resultPoint);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (NotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }  
